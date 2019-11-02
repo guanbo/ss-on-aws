39cd8512a62e7151e948e4fff03e4b41aac45eee
Guan Bo <guanbo2002@gmail.com>
2019-11-02 20:05:44 +0800

    Fixed: install scripts

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ user_host=ec2-user@${host}
 
 ssh-keyscan -H ${host}
 scp server/* ${user_host}:.
-ssh ${user_host} install_ss.sh
+ssh ${user_host} ./install_ss.sh
--- server/install_ss.sh
+++ server/install_ss.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install git golang
+sudo yum install git -y
+sudo amazon-linux-extras install golang1.9
 go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
 nohup $PWD/go/bin/shadowsocks-server &
