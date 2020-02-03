fb0195513140dd62ced94c8a0e57da8b5af3571c
Guan Bo <guanbo2002@gmail.com>
2020-02-04 00:22:37 +0800

    Add: authorize ip

---
+++ authorize_ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+public_ip=$1
+if [ !$public_ip ] 
+then
+  public_ip=`curl -L ip-api.com/line?fields=query`
+fi
+
+export AWS_PROFILE=guanbo
+aws ec2 authorize-security-group-ingress --group-name ss --protocol tcp --port 10200-10300 --cidr ${public_ip}/32
+aws ec2 authorize-security-group-ingress --group-name ss --protocol udp --port 10200-10300 --cidr ${public_ip}/32
--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 host=$1
 user_host=ec2-user@${host}
+# passwd=`openssl rand -base64 48`
+passwd=E2E1D5DC37B54A40963AD10071C55127
+
+# generate configure file
+python -c 'import sys,json;data=json.load(sys.stdin);data["server"]=sys.argv[1];data["password"]=sys.argv[2];print json.dumps(data)' ${host} ${passwd} < config.tpl > server/config.json
 
 ssh-keyscan -H ${host}
-python -c 'import sys,json;data=json.load(sys.stdin);data["server"]=sys.argv[1];print json.dumps(data)' ${host} < config.tpl > server/config.json
 scp server/* ${user_host}:.
 ssh ${user_host} ./install_ss.sh ${host}
+
+echo "ShadowSocks Server configuration following..."
+cat server/config.json
