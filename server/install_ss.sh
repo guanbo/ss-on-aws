8d3a02465aad7eaecc78ce4e144dcd8f665444fa
Guan Bo <guanbo2002@gmail.com>
2020-02-03 19:40:00 +0800

    Update: ss

--- server/install_ss.sh
+++ server/install_ss.sh
@@ -3,8 +3,8 @@
 sudo yum update -y
 sudo yum install git -y
 sudo amazon-linux-extras install golang1.9
-# go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
-go get -u -v github.com/shadowsocks/go-shadowsocks2
+go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
+# go get -u -v github.com/shadowsocks/go-shadowsocks2
 echo "===Install Done==="
 
 ./startup.sh
