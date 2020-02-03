70a588fdb45033dab26534b6e18a81a02823f0dd
Guan Bo <guanbo2002@gmail.com>
2020-02-03 13:24:00 +0800

    Update: go-shadowsocks2

--- server/startup.sh
+++ server/startup.sh
@@ -4,5 +4,6 @@ set -e
 
 ./shutdown.sh
 
-nohup $PWD/go/bin/shadowsocks-server > nohup.out 2>/dev/null &
+# nohup $PWD/go/bin/shadowsocks-server > nohup.out 2>/dev/null &
+nohup $PWD/go/bin/go-shadowsocks2 -s 'ss://AEAD_CHACHA20_POLY1305:E2E1D5DC37B54A40963AD100715127@:10263' -verbose > nohup.out 2>/dev/null &
 echo "===StartUp ShadowSocks Server==="
