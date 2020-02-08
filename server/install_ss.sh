8059e88ab7350341decbb606d7a148fc6eeac7e8
Guan Bo <guanbo2002@gmail.com>
2020-02-08 20:28:43 +0800

    Add: authorize app server

--- server/install_ss.sh
+++ server/install_ss.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install git -y
+sudo yum install git python3 -y
 sudo amazon-linux-extras install golang1.9
 go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-server
 # go get -u -v github.com/shadowsocks/go-shadowsocks2
 echo "===Install Done==="
+
+mkdir -p .aws/
+ec2_region=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/'`
+cat > .aws/config << EOF
+[default]
+output = json
+region = $ec2_region
+EOF
+
+python3 app.py &
 
 ./startup.sh
