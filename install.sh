392d68fe4b4b18efa514fbaa48890d023a7e1b32
GuanBo <guanbo2002@gmail.com>
2021-05-18 13:42:33 +0800

    update install script

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ passwd=E2E1D5DC37B54A40963AD10071C55127
 cert=~/.ssh/mbp16
 
 # generate configure file
-python -c 'import sys,json;data=json.load(sys.stdin);data["server"]=sys.argv[1];data["password"]=sys.argv[2];print(json.dumps(data))' ${host} ${passwd} < config.tpl > server/config.json
+# python -c 'import sys,json;data=json.load(sys.stdin);data["server"]=sys.argv[1];data["password"]=sys.argv[2];print(json.dumps(data))' ${host} ${passwd} < config.tpl > server/config.json
 
 ssh-keyscan -H ${host}
 scp -i ${cert} server/* ${user_host}:.
 ssh -i ${cert} ${user_host} ./install_ss.sh ${host}
 
 echo "ShadowSocks Server configuration following..."
-cat server/config.json
+# cat server/config.json
 
 ./authorize_ip.sh
