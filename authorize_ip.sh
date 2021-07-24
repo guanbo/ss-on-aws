dc7d753f59b4d7259910ae10a3a34170d19010ff
GuanBo <guanbo2002@gmail.com>
2021-07-25 00:26:17 +0800

    update

--- authorize_ip.sh
+++ authorize_ip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+aws_region=${1:-us-west-2}
+
 public_ip=$1
 if [ !$public_ip ] 
 then
@@ -9,5 +11,5 @@ fi
 echo "===Authorize ${public_ip} to security groups ingress==="
 
 export AWS_PROFILE=guanbo
-aws ec2 authorize-security-group-ingress --group-name ss --protocol tcp --port 10200-10300 --cidr ${public_ip}/32
+aws ec2 authorize-security-group-ingress --group-name ss --protocol tcp --port 10200-10300 --cidr ${public_ip}/32 --region ${aws_region}
 #aws ec2 authorize-security-group-ingress --group-name ss --protocol udp --port 10200-10300 --cidr ${public_ip}/32
--- config.tpl
+++ config.tpl
@@ -2,6 +2,6 @@
     "server":"HOST_PUBLIC_IP",
     "server_port":10263,
     "password":"E2E1D5DC37B54A40963AD10071C55127",
-    "method": "chacha20-ietf",
+    "method": "chacha20-ietf-poly1305",
     "timeout":600
 }
