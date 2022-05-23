bad469d7d782fd6a59de4873f3d26f099a02e114
Bo Guan <guanbo2002@gmail.com>
2022-05-23 21:55:18 +0800

    Add: elastic ip

--- spot.sh
+++ spot.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-aws_region=${1:-us-west-2}
-
+export AWS_PROFILE=guanbo
+aws_region=$(aws configure get region)
 echo "spot on ${aws_region}"
 
-export AWS_PROFILE=guanbo
+function get_instance_id()
+{
+  aws ec2 describe-instances --filter Name=instance-lifecycle,Values=spot > instances.json
+  instance_id=`python -c 'import sys,json;data=json.load(sys.stdin);print(data["Reservations"][0]["Instances"][0]["InstanceId"]);' < instances.json`
+}
 
-aws ec2 request-spot-fleet --spot-fleet-request-config file://${aws_region}.json --region ${aws_region}
+function get_public_ip()
+{
+  aws ec2 describe-addresses > addresses.json
+  public_ip=`python -c 'import sys,json;data=json.load(sys.stdin);print(data["Addresses"][0]["PublicIp"]);' < addresses.json`
+}
+
+aws ec2 request-spot-fleet --spot-fleet-request-config file://${aws_region}.json
+
+get_public_ip
+get_instance_id
+aws ec2 wait instance-running --instance-ids $instance_id
+
+echo "associate-address $public_ip to $instance_id "
+aws ec2 associate-address --instance-id $instance_id --public-ip $public_ip
