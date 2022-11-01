6fffeaea1c01417a2946cadad25723eb9f0fc997
Bo Guan <guanbo2002@gmail.com>
2022-11-01 23:58:57 +0800

    fixed: refresh script

--- refresh.sh
+++ refresh.sh
@@ -11,11 +11,11 @@ aws_region=$(aws configure get region)
 echo "refersh on ${aws_region}"
 
 instance=$(aws ec2 describe-instances| jq '.Reservations[0].Instances[0]')
-instance_id=$(echo $instance |jq '.InstanceId')
-associated_ip=$(echo $instance|jq '.NetworkInterfaces[0].Association.PublicIp')
-allocated_id=$(aws ec2 describe-addresses| jq '.Addresses[0].AllocationId')
+instance_id=$(echo $instance |jq '.InstanceId' | sed 's/"//g')
+associated_ip=$(echo $instance|jq '.NetworkInterfaces[0].Association.PublicIp' | sed 's/"//g')
+allocated_id=$(aws ec2 describe-addresses| jq '.Addresses[0].AllocationId' | sed 's/"//g')
 
-echo "instance_id: $instance_id"
+echo "instance_id: ${instance_id}"
 echo "associated_ip: $associated_ip"
 echo "allocated_id: $allocated_id"
 
@@ -26,7 +26,9 @@ echo "allocated_id: $allocated_id"
 #     "NetworkBorderGroup": "us-west-2",
 #     "Domain": "vpc"
 # }
-public_ip=$(aws ec2 allocate-address | jq '.PublicIp')
+public_ip=$(aws ec2 allocate-address | jq '.PublicIp' | sed 's/"//g')
 echo "public_ip: $public_ip"
+echo "aws ec2 associate-address --instance-id $instance_id --public-ip $public_ip"
 aws ec2 associate-address --instance-id $instance_id --public-ip $public_ip
 aws ec2 release-address --allocation-id $allocated_id
+aws ec2 describe-addresses
